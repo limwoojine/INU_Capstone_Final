1e4d0b5c5f87c9f98ca85759b1411a5b823c1bae
limwoojine <86238485+limwoojine@users.noreply.github.com>
2022-03-12 02:27:58 +0900

    2022/03/12 Dialog 및 Listener 추가

---
+++ app/src/main/java/com/example/capstone/AllReviewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.capstone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AllReviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_review)
+    }
+}
